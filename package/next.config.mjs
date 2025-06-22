b46c075347c5ceb5399530ca7c5397679f64596f
CEO <ceo@map2map.com>
2025-06-23 04:59:23 +0530

    adding next config

--- package/next.config.mjs
+++ package/next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'aiponatime';
+
 const nextConfig = {
+  output: 'export',
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 export default nextConfig
