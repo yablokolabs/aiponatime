e5f89435246a33556172d112411a71a26c9e4c72
CEO <ceo@map2map.com>
2025-06-23 06:11:57 +0530

    use custom domain

--- package/next.config.js
+++ package/next.config.js
@@ -1,14 +1,7 @@
-// package/next.config.js
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const repo   = 'aiponatime';
-
 const nextConfig = {
   output: 'export',
-  basePath:   isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
   images: { unoptimized: true },
-  env: { NEXT_PUBLIC_BASE_PATH: isProd ? `/${repo}` : '' },
   trailingSlash: true,
 };
 
